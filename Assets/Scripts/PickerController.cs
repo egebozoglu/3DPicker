105c988e4be8961e862bb900968882791d5fe26a
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-10-11 10:41:09 +0300

    Added Basic Picker Controller
    + create: basic picker controller

--- Assets/Scripts/PickerController.cs
+++ Assets/Scripts/PickerController.cs
@@ -2,17 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Picker3D.Picker{
+    public class PickerController : MonoBehaviour
     {
-        
-    }
+        #region Variables
+        [Header("Movement")]
+        private Rigidbody rb;
+        [SerializeField] private bool playable = false;
+        [SerializeField] private float speed = 5;
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            rb = transform.GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            if (playable)
+            {
+                rb.MovePosition(rb.position + (Vector3.forward * speed * Time.deltaTime));
+            }
+
+            int i = 0;
+
+            //loop over every touch found    
+            while (i < Input.touchCount)
+            {
+                if (Input.GetTouch(i).position.x > Screen.width / 2)
+                {
+                    //move right    
+                    HorizontalMove(true);
+                }
+                if (Input.GetTouch(i).position.x < Screen.width / 2)
+                {
+                    //move left    
+                    HorizontalMove(false);
+                }
+                ++i;
+            }
+        }
+
+        private void HorizontalMove(bool right)
+        {
+            float rightMove = 1;
+            if (!right)
+            {
+                rightMove = -1;
+            }
+            rb.MovePosition(rb.position + (Vector3.right * rightMove * speed * Time.deltaTime));
+        }
     }
 }
