443fd6393e47c9cdf3f907b2fb98683dfe9d27e0
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-10-13 23:08:10 +0300

    Add Screens
    + feat: screens created

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -6,6 +6,13 @@ namespace Picker3D.Manager
 {
     public class UIManager : MonoBehaviour
     {
+        #region Variables
+        [Header("Screens")]
+        [SerializeField] private GameObject startScreen;
+        [SerializeField] private GameObject gameScreen;
+        [SerializeField] private GameObject endScreen;
+        #endregion
+
         // Start is called before the first frame update
         void Start()
         {
