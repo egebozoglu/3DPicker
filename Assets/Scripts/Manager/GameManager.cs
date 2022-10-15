8f7f7fd0bdb5503d4fff48061ff84b2fd504ede7
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-10-15 12:58:22 +0300

    Update Game Manager
    + refactor: game mechanics being developed

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,9 @@
 using Picker3D.Camera;
+using Picker3D.EditorScene;
+using Picker3D.Platform;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Picker3D.Manager
@@ -17,9 +20,16 @@ namespace Picker3D.Manager
         [SerializeField] private GameObject pickerPrefab;
         [SerializeField] private GameObject mainCamera;
 
+        [Header("Platform")]
+        [SerializeField] private GameObject platformBasePrefab;
+
         [Header("Game Mechanics")]
-        public int TotalCoins = 0;
+        [HideInInspector] public int TotalCoins = 0;
         [SerializeField] private GameManagerData gameManagerData;
+
+        [Header("Level")]
+        private LevelScriptable levelScriptable;
+        [SerializeField] private List<GameObject> collectibleObjectPrefabs = new List<GameObject>();
         #endregion
 
         private void Awake()
@@ -44,7 +54,10 @@ namespace Picker3D.Manager
         void Start()
         {
             SetSkybox();
+            LoadLevel();
+            InstantiatePlatform();
             InstantiatePicker();
+            InstantiateCollectibles();
         }
 
         // Update is called once per frame
@@ -61,12 +74,63 @@ namespace Picker3D.Manager
             RenderSettings.skybox = material;
         }
 
+        private void LoadLevel()
+        {
+            int level = PlayerPrefs.GetInt("Level");
+            List<LevelScriptable> levelScriptables = new List<LevelScriptable>();
+            Object[] leveldata = Resources.LoadAll<LevelScriptable>("Levels");
+            foreach (var item in leveldata)
+            {
+                levelScriptables.Add((LevelScriptable)item);
+            }
+
+            if (level > levelScriptables.Count)
+            {
+                System.Random rand = new System.Random();
+                levelScriptable = levelScriptables[rand.Next(levelScriptables.Count)];
+            }
+            else
+            {
+                levelScriptable = levelScriptables.Where(x => x.name == "Level" + level.ToString()).FirstOrDefault();
+            }
+            Debug.Log(levelScriptable.AllObjects.Count.ToString());
+            Debug.Log(levelScriptable.CompleteCounts.Count.ToString());
+            Debug.Log(levelScriptable.PlatformColor);
+        }
+
+        private void InstantiatePlatform()
+        {
+            GameObject platform;
+
+            platform = Instantiate(platformBasePrefab);
+            var platformController = platform.GetComponent<PlatformController>();
+            platformController.ItemsColor = levelScriptable.PlatformColor;
+            platformController.CompleteCounts = levelScriptable.CompleteCounts;
+            platformController.PlatformSettings();
+        }
+
         private void InstantiatePicker()
         {
             GameObject picker;
 
             picker = Instantiate(pickerPrefab);
             mainCamera.GetComponent<CameraController>().Picker = picker;
+        }
+
+        private void InstantiateCollectibles()
+        {
+            foreach (LevelObject levelObject in levelScriptable.AllObjects)
+            {
+                GameObject prefab = collectibleObjectPrefabs[0];
+                for (int i = 0; i < collectibleObjectPrefabs.Count; i++)
+                {
+                    if (levelObject.ObjectName == collectibleObjectPrefabs[i].name)
+                    {
+                        prefab = collectibleObjectPrefabs[i];
+                    }
+                }
+                Instantiate(prefab, levelObject.Position, levelObject.Rotation);
+            }
         }
     }
 }
